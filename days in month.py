09ffd55a989ece0846d0c2e44dd4c4329563cc07
spoddub <108150454+spoddub@users.noreply.github.com>
2022-07-03 13:57:03 +0300

    Add files via upload

---
+++ days in month.py
@@ -0,0 +1,24 @@
+# my first kinda program on python
+# lets tell user hwo many days in month of their choice
+user_input = input("please enter month number: ")
+month = int(user_input)
+if month == 1:
+    print('31')
+elif month == 3:
+    print(31)
+elif month == 5:
+    print(31)
+elif month == 7:
+    print(31)
+elif month == 8:
+    print(31)
+elif month == 10:
+    print(31)
+elif month == 12:
+    print(31)
+elif month == 2:
+    print(28)
+else:
+    print(30)
+
+print('you entered', month)
