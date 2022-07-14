b3308ef8cc1b24c3816cd822a5db148aa4825ab8
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-14 18:13:19 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,4 +1,5 @@
 n = int(input())
-for i in range(n):
-    for j in range(5):
-        print(i, end=' ')
+for i in range(1, n + 1):
+    for j in range(9):
+        print(n, '+', i, '=', n + i)
+    print()
