9dcaa8db5cd4bf26b440613a29e6e484a80ef41e
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-16 17:34:24 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,9 +1,5 @@
 s = input()
-print(s[2])
-print(s[-2])
-print(s[:5])
-print(s[:-2])
-print(s[::1])
-print(s[::2])
-print(s[::-1])
-print(s[:-1:2])
+if s == s.title():
+    print('YES')
+else:
+    print('NO')
