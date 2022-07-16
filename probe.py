141589c44c11043e313136c3fbb3572d58d3023d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-16 19:50:44 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,7 +1,10 @@
-s = input()
-s1 = s.lower()
-print('Аденин:', s.count('а'))
-print('Гуанин:', s1.count('г'))
-print('Цитозин:', s1.count('ц'))
-print('Тимин:', s1.count('т'))
+s = int(input())
+total = 0
+for i in s:
+    s1 = input()
+    if '11' in s1:
+        total += 1
+print(total)
+
+
 
