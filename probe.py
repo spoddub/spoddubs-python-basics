86bc3502ce1c4eda30e86c24ca398a9654801f26
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-17 16:00:52 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,11 +1,9 @@
 s = input()
-count = 0
-letter = 0
-for i in s:
-    if s.count(i) >= count:
-        count = s.count(i)
-        letter = i
-print(i)
-
+if s.count('f') == 1:
+    print(s.find('f'))
+elif s.count('f') >= 2:
+    print(s.find('f'), s.rfind('f'))
+else:
+    print('NO')
 
 
