207ec014626c2f5138b7141752292411208b3cb0
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-17 17:23:25 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,6 +1,15 @@
+n = int(input())
 s = input()
-for i in range(int(len(s))):
-    print(ord(i), end=' ')
+for i in range(len(s)):
+    f = s[i]
+    a = ord(s[i]) + (26 - n)
+    if 97 <= a <= 122:
+        print(chr(a), end='')
+        continue
+    if a > 122:
+        print(chr(96 + (a - 122)), end='')
+        continue
+
 
 
 
