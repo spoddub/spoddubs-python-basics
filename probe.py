68d3b79acb5def614593efb54914c1038b358eb2
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-18 17:02:09 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,9 +1,17 @@
 n = int(input())
 my_list = []
 for _ in range(n):
-    stroka = input()
-    my_list.extend(stroka)
-print(my_list)
+    m = int(input())
+    my_list.append(m)
+print(*my_list, sep='\n')
+print()
+new_list = []
+for num in my_list:
+    number = num ** 2 + num * 2 + 1
+    new_list.append(number)
+print(*new_list, sep='\n')
+
+
 
 
 
