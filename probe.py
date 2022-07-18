2fdfba6f5d767bccc10ce04349f748e95cbe6c34
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-18 19:36:19 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,12 +1,20 @@
 n = int(input())
 my_list = []
 for _ in range(n):
-    stroka = input()
-    my_list.append(stroka)
-k = input()
-for searchies in my_list:
-    if k in searchies or k.title() in searchies or k.swapcase() in searchies:
-        print(searchies)
+    m = input()
+    my_list.append(m)
+k = int(input())
+new_list = []
+for i in range(k):
+    stroka = input().lower()
+    if stroka in my_list.lower:
+        new_list.append(stroka)
+    
+
+
+
+
+
 
 
 
