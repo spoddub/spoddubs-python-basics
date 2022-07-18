0107b3f3aaa79fc17dbeb15af04cc28ce63515e4
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-18 13:22:37 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -1,8 +1,8 @@
 n = int(input())
 my_list = []
-for i in range(n):
-    m = int(input())
-    my_list.append(m ** 3)
+for i in range(1, n + 1):
+    if n % i == 0:
+        my_list.append(i)
 print(my_list)
 
 
