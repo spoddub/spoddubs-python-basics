c7c9ce48ebe9e491d7c6c603c4f8e6c8de6a17d6
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-18 17:22:08 +0300

    решил задачу

--- probe.py
+++ probe.py
@@ -3,13 +3,11 @@ my_list = []
 for _ in range(n):
     m = int(input())
     my_list.append(m)
+my_list.remove(min(my_list))
+my_list.remove(max(my_list))
 print(*my_list, sep='\n')
-print()
-new_list = []
-for num in my_list:
-    number = num ** 2 + num * 2 + 1
-    new_list.append(number)
-print(*new_list, sep='\n')
+
+
 
 
 
