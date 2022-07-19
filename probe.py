99b613ab18d8f4e7d1298f6f628db49d18805ace
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-19 10:33:13 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,18 +1,6 @@
-n = int(input())
-Negatives = []
-Zeros = []
-Positives = []
-for _ in range(n):
-    m = int(input())
-    if m > 0:
-        Positives.append(m)
-    elif m == 0:
-        Zeros.append(m)
-    else:
-        Negatives.append(m)
-print(*Negatives, sep='\n')
-print(*Zeros, sep='\n')
-print(*Positives, sep='\n')
+s = input()
+words = s.split()
+print(words, sep='\n')
 
 
 
