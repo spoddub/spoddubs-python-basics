0fb8dcb58dfbb636bf9a7d304a8013961d9b18eb
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-19 17:33:08 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,11 +1,6 @@
-n = input()
-for _ in range(int(n[1:])):
-    s = input()
-    if '#' in s:
-        s = s[:s.find('#')]
-    print(s.rstrip())
-
-
+s = int(input())
+print(s.sort())
+print(s.sort(reverse = True))
 
 
 
