b388187164cbdfd832d24cdbc834b2f1ea4f6002
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-19 17:01:37 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,13 +1,9 @@
-numbers = [8, 9, 10, 11]
-numbers.remove(numbers[1])
-numbers.insert(1, int(17))
-numbers.append(int(4))
-numbers.append(int(5))
-numbers.append(int(6))
-numbers.remove(numbers[0])
-numbers *= 2
-numbers.insert(3, int(25))
-print(numbers)
+n = input()
+for _ in range(int(n[1:])):
+    s = input()
+    if '#' in s:
+        s = s[:s.find('#')]
+    print(s.rstrip())
 
 
 
