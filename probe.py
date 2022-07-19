4d75fa2977cbf41a237f768a3b2767a61dcbb1c5
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-20 02:24:06 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,15 +1,14 @@
-def get_factors(num):
-    my_list = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            my_list.append(i)
-    return len(my_list)
+def find_all(target, symbol):
+    li = []
+    for i in range(len(target)):
+        if target[i] == symbol:
+            li.append(i)
+    return li
 
+s = input()
+char = input()
 
-n = int(input())
-
-print(get_factors(n))
-
+print(find_all(s, char))
 
 
 
