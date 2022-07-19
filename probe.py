70eef6f9a41cce365a8c6034f4ae08dcce57961b
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-19 20:26:30 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,10 +1,5 @@
-s = input()
-numbers = s.split()
-new_numbers = []
-for i in numbers:
-    x = int(i) ** 3
-    new_numbers.append(x)
-print(*new_numbers)
+print(*(i for i in input() if i.isdigit()), sep='')
+
 
 
 
