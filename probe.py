7fd24a8e16a09297ee26483016e58f2234797b3e
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-20 20:06:21 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,23 +1,8 @@
-# объявление функции
-def is_one_away(word1, word2):
-    if len(word1) == len(word2):
-        counter = 0
-        for i in range(len(word1)):
-            if word1[i] != word2[i]:
-                counter += 1
-        if counter == 0 or counter >= 2:
-            return False
-        else:
-            return True
-    else:
-        return False
+def is_palindrome(text):
+    text = text.lower()
+    a = [c for c in text if c.isalpha()]
+    return a == a[::-1]
 
+txt = input()
 
-
-
-# считываем данные
-txt1 = input()
-txt2 = input()
-
-# вызываем функцию
-print(is_one_away(txt1, txt2))
+print(is_palindrome(txt))
