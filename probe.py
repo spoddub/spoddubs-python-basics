f244836fcdb9518a8504e2335cdbd060a76fff2d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-20 21:55:25 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,10 +1,15 @@
-def convert_to_python_case(text):
-    s = ''
-    for el in text:
-        if el.isupper():
-            s += '_'
-        s += el.lower()
-    return s[1:]
+# объявление функции
+def get_middle_point(x1, y1, x2, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return x, y
 
 
-print(convert_to_python_case(input()))
+
+# считываем данные
+x_1, y_1 = int(input()), int(input())
+x_2, y_2 = int(input()), int(input())
+
+# вызываем функцию
+x, y = get_middle_point(x_1, y_1, x_2, y_2)
+print(x, y)
