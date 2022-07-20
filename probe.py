cfbd70762d1e4f2e7c6e96d08d45f3c69ef5fff6
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-20 20:33:50 +0300

    снова решенная задача

--- probe.py
+++ probe.py
@@ -1,22 +1,11 @@
-def is_palindrome(text):
-    text = text.lower()
-    a = [c for c in text if c.isalpha()]
-    return a == a[::-1]
+# объявление функции
+def is_correct_bracket(text):
+    while '()' in text:
+        text.replace('()', '')
+    return text == ''
 
+# считываем данные
+txt = input()
 
-def is_prime(num):
-    if num == 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    else:
-        return True
-
-
-def is_even(num):
-    return num % 2 == 0
-
-
-def is_valid_password(password):
-    return is_even() and
+# вызываем функцию
+print(is_correct_bracket(txt))
